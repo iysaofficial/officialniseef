9aa1fb1e5c916807ebdf9d405e11346efa5972f3
young <youngscientist.iysa@gmail.com>
2025-01-07 11:09:52 +0700

    close

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ class Footer extends Component {
                   <div className="widget widget_services border-0">
                     <ul>
                       <li>
-                        <Link href="/registration/homeregist">Pendaftaran</Link>
+                        <Link href="/">Tutup Pendaftaran</Link>
                       </li>
                       {/* <li>
                         <Link href="/#Testimonial">Testimonials</Link>
